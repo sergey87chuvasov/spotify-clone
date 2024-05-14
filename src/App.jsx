50cc8300a541a10641bd0b50aa66b4ae1944471a
Sergey Chv <serge.techno@gmail.com>
2024-05-14 18:41:00 +0300

    Create Album List and Song List - 1 part

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-
 import './App.css'
+import Display from './components/Display'
 import Player from './components/Player'
 import Sidebar from './components/Sidebar'
 
 const App = () => {
  
-
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <Sidebar />
+        <Display />
       </div>
       <Player />
     </div>
---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+
+const DisplayHome = () => {
+ 
+    return (
+      <>
+        <Navbar />
+      </>
+    )
+  }
+  
+export default DisplayHome;
+  
