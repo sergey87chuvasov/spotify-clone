762cdf9cf22b07f39def264cbca57bfa507f81d3
Sergey Chv <serge.techno@gmail.com>
2024-05-17 00:27:38 +0300

    added current time and song duration

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,7 +4,7 @@ import { PlayerContext } from "../context/PlayerContext"
 
 const Player= () => {
 
-    const { track, seekBar, seekBg, playStatus, play, pause } = useContext(PlayerContext)
+    const { track, seekBar, seekBg, playStatus, play, pause, time } = useContext(PlayerContext)
  
 
   return (
@@ -27,11 +27,11 @@ const Player= () => {
                 <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="img pic" />
             </div>
             <div className="flex items-center gap-5">
-                <p>1:44</p>
+                <p>{time.currentTime.minute}:{time.currentTime.second}</p>
                 <div ref={seekBg} className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
                     <hr ref={seekBar} className="h-1 border-none w-20 bg-green-800 rounded-full" />
                 </div>
-                <p>3:55</p>
+                <p>{time.totalTime.minute}:{time.totalTime.second}</p>
             </div>
         </div>
         <div className="hidden lg:flex items-center gap-2 opacity-75">
