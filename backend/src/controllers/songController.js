686e982ef3d90a5e1f1923d4e10096497f264b2b
Sergey87 <serge.techno@gmail.com>
2024-07-25 00:19:50 +0300

    work with backend - 3 part

--- backend/src/controllers/songController.js
+++ backend/src/controllers/songController.js
@@ -1,5 +1,28 @@
-const addSong = async (req, res) => {};
+import { v2 as cloudinary } from 'cloudinary';
+import songModel from '../models/songModel.js';
 
-const listSong = async (req, res) => {};
+const addSong = async (req, res) => {
+  try {
+    const name = req.body.name;
+    const desc = req.body.desc;
+    const album = req.body.album;
+    const audioFile = req.files.audio[0];
+    const imageFile = req.files.image[0];
+
+    const audioUpload = await cloudinary.uploader.upload(audioFile.path, {
+      resource_type: 'video',
+    });
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: 'image',
+    });
+
+    console.log(name, desc, album, audioUpload, imageUpload);
+  } catch (err) {}
+};
+
+const listSong = async (req, res) => {
+  try {
+  } catch (err) {}
+};
 
 export { addSong, listSong };
